ad743f7bc934fdacde2a27d0bc0794ee0ed0b548
Mateusz Jablonski <mjablonski.praca@gmail.com>
2022-10-25 17:50:28 +0200

    code 25.10.2022 17:50

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+import {useEffect, useState} from 'react'
+
+const HomePage = () => {
+    useEffect(()=>{}, )
+    
+    // (, []) - lista dependencji - funkcja wykona się tylko raz, a później nie będzie reagować na zmianę stanu
+    // drugi argument:
+    // 1. Nie podawać w ogóle
+    // 2. Podać pustą listę
+    // 3. Podać zapełnioną listę
+    return (
+        <div>HomePage</div>
+    )
+}
+
+export default HomePage
+
+// Ściąganie newsów z API oraz ich wyświetlanie
+// useEffect
+// axios
+
+// Praca z API w React
+// fetch, axios, React Query?
+// axios to bardziej rozbudowany fetch
